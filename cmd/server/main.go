93be762c16a41ad960e84e9af87306284d17ff11
antonymaks <antonymaks@yandex.ru>
2026-09-01 19:52:00 +0300

    Server added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Server started")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", homeHandler)
+	mux.HandleFunc("GET /health", healthHandler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	fmt.Println("Server started on http://localhost:8080")
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	fmt.Fprint(w, `{"message":"Book Catalog API"}`)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	fmt.Fprint(w, `{"status":"ok"}`)
 }
